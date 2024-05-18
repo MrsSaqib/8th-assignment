11f1bf226b498666cbb8b84440e21dbbfef555f7
MrsSaqib <157152885+MrsSaqib@users.noreply.github.com>
2024-05-19 00:03:40 +0500

    Add files via upload

---
+++ chapter 2/variables.js
@@ -0,0 +1,23 @@
+let username = "Humera";
+alert(username);
+let message = "Hello World";
+alert(message)
+
+let x = "Humera"
+alert(x)
+let y = 40
+
+alert(y + " years old")
+let z = "Certified Mobile Application Development"
+alert(z)
+
+alert("PIZZA \nPIZZ\nPIZ\nPI")
+
+let email = "humas.saqib@gmail.com"
+alert("My email is " + email)
+
+let book = "A smarter way to learn JavaScript."
+alert("I am trying to learning from a book" + book)
+document.write("Yah! I can write HTML content through Javascript")
+let a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(a)
